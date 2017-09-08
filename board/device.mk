bfa14263ce8c9bf4e8523a25f1aeefef83a7b76d
Mohan CM <mohanmanjappa@gmail.com>
2017-09-08 21:52:22 +0530

    Twrp-ify(initial build)

---
+++ board/device.mk
@@ -0,0 +1,10 @@
+# OTA assert
+TARGET_OTA_ASSERT_DEVICE := CP8676_I02,note3,COOLPAD,coolpad
+
+# Bionic
+
+# Enable Minikin text layout engine (will be the default soon)
+USE_MINIKIN := true
+
+#Use dlmalloc instead of jemalloc for mallocs
+MALLOC_IMPL := dlmalloc
